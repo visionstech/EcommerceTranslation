b26a062ca2db4d6a4c2ad6002656681993361ae8
Visions <odesk.visions@gmail.com>
2017-01-18 10:18:12 +0530

    up

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
  * @package  Laravel
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
-echo "sss";
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
