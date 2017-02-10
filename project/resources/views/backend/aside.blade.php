659cc919787e2e3c652610b5c4b5d47f75317bb6
Visions <odesk.visions@gmail.com>
2017-02-10 19:18:47 +0530

    up

--- project/resources/views/backend/aside.blade.php
+++ project/resources/views/backend/aside.blade.php
@@ -3,7 +3,7 @@
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
       <!-- Sidebar user panel -->
-      <div class="user-panel">
+     <!-- <div class="user-panel">
         <div class="pull-left image">
           <img src="{{ asset('/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
         </div>
@@ -11,10 +11,10 @@
           <p>Alexander Pierce</p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
-      </div>
+      </div>-->
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">
-        <li class="header">MAIN NAVIGATION</li>
+       
        
         <li class="{{ (strpos($_SERVER['REQUEST_URI'],'dashboard') != false)?'active':'' }}">
           <a href="{{ url('/dashboard') }}">
--- project/storage/framework/views/893c54ca43ff8f6eabc8868ac85ae5128a1a3ca6.php
+++ project/storage/framework/views/893c54ca43ff8f6eabc8868ac85ae5128a1a3ca6.php
@@ -62,13 +62,12 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="<?php echo e(asset('/img/user2-160x160.jpg')); ?>" class="user-image" alt="User Image">
+             
               <span class="hidden-xs">Alexander Pierce</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
-              <li class="user-header">
-                <img src="<?php echo e(asset('/img/user2-160x160.jpg')); ?>" class="img-circle" alt="User Image">
+              <li class="user-header">         
                 <p>
                   Alexander Pierce - Web Developer
                   <small>Member since Nov. 2012</small>
